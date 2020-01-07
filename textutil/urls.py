77705f17d0691b2e63b909ec73f7d7feca4543a7
Ankur <ankurrathi1611@gmail.com>
2020-01-08 04:04:48 +0530

    comments

--- textutil/urls.py
+++ textutil/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('analyze', views.analyze, name="analyze"),
 
 
+
 ]
--- textutil/views.py
+++ textutil/views.py
@@ -4,14 +4,14 @@ from django.shortcuts import render
 """
 # code for previous vedio
 
-#def about(request):
-    #return HttpResponse('<h2>About Ankur</h2> <a href="https://www.youtube.com/">Youtube</a>')
-#def text(request): # read the text file through django
-    #file=open("textutil/text1.txt",'r+')
-    #return HttpResponse(file.read())
-#def navbar(request): # for navigation bar
-    #nav = '<h1>Navigation</h1> <ul> <li><a href="https://www.youtube.com/playlist?list=PL8p2I9GklV47fi-yiWkfRpbMV8PPaQDH4">Laravel Playlist</a></li> <li><a href="https://www.youtube.com/playlist?list=PLLgihBgNlnFS-cJ7j2-I6WvyQKuRNbqUT">Python By buildinggame</a></li> <li><a href="https://www.youtube.com/playlist?list=PLd3UqWTnYXOkY3Ajiydn_Ok9etnwVv8Hx">Django Rest API</a></li></ul>'
-    #return HttpResponse(nav)
+def about(request):
+    return HttpResponse('<h2>About Ankur</h2> <a href="https://www.youtube.com/">Youtube</a>')
+def text(request): # read the text file through django
+    file=open("textutil/text1.txt",'r+')
+    return HttpResponse(file.read())
+def navbar(request): # for navigation bar
+    nav = '<h1>Navigation</h1> <ul> <li><a href="https://www.youtube.com/playlist?list=PL8p2I9GklV47fi-yiWkfRpbMV8PPaQDH4">Laravel Playlist</a></li> <li><a href="https://www.youtube.com/playlist?list=PLLgihBgNlnFS-cJ7j2-I6WvyQKuRNbqUT">Python By buildinggame</a></li> <li><a href="https://www.youtube.com/playlist?list=PLd3UqWTnYXOkY3Ajiydn_Ok9etnwVv8Hx">Django Rest API</a></li></ul>'
+    return HttpResponse(nav)
 """
 def index(request):
     return render(request, 'index.html')
@@ -20,6 +20,8 @@ def index(request):
 def analyze(request):
     djtext=request.GET.get('text','default')
     punctuate = request.GET.get('punctuation', 'off')
+    charcount = request.GET.get('charcount', 'off')
+
     print(djtext)
     if punctuate == "on":
         punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -27,6 +29,17 @@ def analyze(request):
         for char in djtext:
             if char not in punctuation:
                 analyzed += char
+
+    elif(charcount == 'on'):
+        punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuation:
+                analyzed += char
+        params = {'purpose': 'Counted Character', 'analyzed_text': 'The number of characters are ' + str(len(analyzed))}
+        return render(request, 'analyze.html', params)
+
+
     else:
         return HttpResponse("Please!! tick the checkbox.")
 
